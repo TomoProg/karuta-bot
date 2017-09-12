734a68e9b8ce83205e5c5e8a3ed0ab090a6d1328
TomoProg <helloworld0306.xxx@gmail.com>
2017-09-13 01:53:34 +0900

    Slack通知機能を追加

--- app/karuta_bot.py
+++ app/karuta_bot.py
@@ -1,5 +1,6 @@
 #-*- coding:utf-8 -*-
 from twitter import * # GitHub: https://github.com/sixohsix/twitter
+import slackweb       # GitHub: https://github.com/satoshi03/slack-python-webhook
 import json
 import datetime
 import time
@@ -18,6 +19,7 @@ app_auth = None
 conf_file_name = "conf.json"
 karuta_info_file_name = "karuta_info.json"
 logger = logger.Logger(__file__ + ".log")
+slack = None
 
 def main():
     """ main """
@@ -27,6 +29,7 @@ def main():
     global app_auth
     global conf_file_name
     global karuta_info_file_name
+    global slack
 
     #-------------------------------------------------------------
     # 設定ファイル読み込み
@@ -35,9 +38,15 @@ def main():
         with open(conf_file_name) as f:
             conf_data = json.loads(f.read())
     except Exception as e:
-        logger.write("設定ファイルの読み込みに失敗しました。")
-        logger.write(str(e))
+        print("{0}の読み込みに失敗しました。".format(conf_file_name))
+        print(str(e))
         sys.exit(1)
+
+    #-------------------------------------------------------------
+    # SlackのWebHookURL読み込み
+    #-------------------------------------------------------------
+    if conf_data["slack_webhook_url"]:
+        slack = slackweb.Slack(url=conf_data["slack_webhook_url"])
 
     #-------------------------------------------------------------
     # Twitterアプリ認証
@@ -51,12 +60,12 @@ def main():
         app = Twitter(auth=app_auth)
         app_up = Twitter(domain="upload.twitter.com", auth=app_auth)
     except Exception as e:
-        logger.write("Twitterアプリ認証に失敗しました。")
-        logger.write(str(e))
+        print("Twitterアプリ認証に失敗しました。")
+        print(str(e))
         sys.exit(1)
 
     #-------------------------------------------------------------
-    # 百人一首ファイル読み込み
+    # 百人一首情報ファイル読み込み
     #-------------------------------------------------------------
     try:
         karuta_info = []
@@ -64,8 +73,8 @@ def main():
             for line in json.load(f):
                 karuta_info.append(line)
     except Exception as e:
-        logger.write("百人一首ファイル[{filename}]の読み込みに失敗しました。".format(filename=karuta_info_file_name))
-        logger.write(str(e))
+        print("{0}の読み込みに失敗しました。".format(karuta_info_file_name))
+        print(str(e))
         sys.exit(1)
 
     #-------------------------------------------------------------
@@ -113,7 +122,8 @@ def karuta_tweet(karuta_info):
         with open(file_path, "rb") as image_f:
             image_data = image_f.read()
     except Exception as e:
-        logger.write("画像ファイル[{filename}]の読み込みに失敗しました".format(filename=filepath))
+        slack_simple_notify("かるた画像の読み込みに失敗しました。ログを確認してください。")
+        logger.write("かるた画像読み込みエラー ファイル名[{0}]".format(filepath))
         logger.write(str(e))
         return False
 
@@ -132,7 +142,8 @@ def karuta_tweet(karuta_info):
         id_img = app_up.media.upload(media=image_data)["media_id_string"]
         app.statuses.update(status=tweet_msg, media_ids=id_img)
     except Exception as e:
-        logger.write("画像のTweetに失敗しました。")
+        slack_simple_notify("原文のツイートに失敗しました。ログを確認してください。")
+        logger.write("原文ツイートエラー")
         logger.write(str(e))
         return False
 
@@ -149,12 +160,23 @@ def karuta_tweet(karuta_info):
     try:
         app.statuses.update(status=tweet_msg)
     except Exception as e:
-        logger.write("現代語訳のTweetに失敗しました。")
+        slack_simple_notify("現代語訳のツイートに失敗しました。ログを確認してください。")
+        logger.write("現代語訳ツイートエラー")
         logger.write(str(e))
         return False
 
     return True
 
+def slack_simple_notify(msg):
+    """ Slackへの通知 """
+    # URLが設定されていない場合は何も行わない
+    global slack
+    if slack:
+        try:
+            slack.notify(text=msg)
+        except Exception as e:
+            logger.write("Slackの通知に失敗しました。WebHookURLを確認してください。")
+            logger.write(str(e))
 
 if __name__ == "__main__":
     main()
